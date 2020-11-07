21a12a71ea6f79c174c5644bd9c1f239ac6f378e
v_glguo_x <981057140@qq.com>
2020-11-07 15:08:49 +0800

    3

--- src/main.js
+++ src/main.js
@@ -5,12 +5,15 @@ import store from './store'
 
 Vue.config.productionTip = false
 function bbb(){
-  console.log(123456,"我是v_glguo写的")
+  // console.log(123456,"我是v_glguo写的")
+  console.log(87444)
 }
 
 function aaa(){
   // console.log('我是v_glguo_x写的，给亮老比演示的代码')
   console.log(111)
+  // console.log(12345679,'我是v_glguo_x写的')
+  console.log(123456)
 }
 aaa()
 
